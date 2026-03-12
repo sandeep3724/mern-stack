cd50da08f6fc0f03d1907952f76ca44d52b03bd2
Sandeep Ankalapu <ankalapusandeep17@gmail.com>
2026-03-12 15:11:45 +0530

    fixed cors for production

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ const app = express();
 
 /* ================= MIDDLEWARE ================= */
 
-app.use(cors());
+app.use(cors({
+origin:"*"
+}));
 app.use(express.json());
 
 /* ================= ROUTES ================= */
